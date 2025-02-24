72cd249143f2e3f454e4bc9862d119e9592bbd1a
takeru-ja0112 <takeruchandesu@gmail.com>
2025-02-24 19:32:41 +0900

    socket setting

--- server/index.js
+++ server/index.js
@@ -7,12 +7,12 @@ const server = http.createServer(app);
 const { Server } = require('socket.io');
 const io = new Server(server, {
     cors: {
-        origin: ["http://localhost:3000"]
+        origin: ["https://main.d1bje3iesvq25w.amplifyapp.com/"]
     }
 });
 
 
-const PORT = 5000;
+const PORT = process.env.PORT || 80;
 
 // クライアントから通信
 io.on('connection', (socket) => {
@@ -34,4 +34,4 @@ io.on('connection', (socket) => {
     });
 })
 
-server.listen(PORT, () => console.log(`server is running on port ${PORT}`))
+server.listen(PORT, () => console.log(`server is running ...`))
