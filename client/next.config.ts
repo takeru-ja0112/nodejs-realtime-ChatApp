76b5f0e7a48d013c08088c9ec6028b06bde100ee
takeru-ja0112 <takeruchandesu@gmail.com>
2025-02-24 12:30:28 +0900

    Amplify setting

--- client/next.config.ts
+++ client/next.config.ts
@@ -2,6 +2,8 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  output: 'export',//Next.jsを静的サイトとして出力
+  trailingSlash:true,//すべてのページでスラッシュを追加
 };
 
 export default nextConfig;
